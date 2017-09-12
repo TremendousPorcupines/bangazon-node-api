f2055a3b03b124b07219cedb34523706c0c97919
Jufebrown <jufebrown@gmail.com>
2017-09-12 15:30:51 -0500

    fixed payment types generator

--- db/build-bangazon-db.js
+++ db/build-bangazon-db.js
@@ -61,7 +61,7 @@ dropEmployeesComputersTable();
 db.run('CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, address TEXT, city TEXT, zip_code TEXT, phone TEXT, date_created TEXT, last_login TEXT)');
 
 // create payment_types table
-db.run('CREATE TABLE IF NOT EXISTS payment_types (payment_type_id INTEGER PRIMARY KEY, name TEXT, account_number TEXT, FOREIGN KEY (user_id) REFERENCES users(user_id))');
+db.run('CREATE TABLE IF NOT EXISTS payment_types (payment_type_id INTEGER PRIMARY KEY, name TEXT, account_number TEXT, user_id INTEGER, FOREIGN KEY (user_id) REFERENCES users(user_id))');
 
 // create orders table
 db.run('CREATE TABLE IF NOT EXISTS orders (order_id INTEGER PRIMARY KEY, FOREIGN KEY (user_id) REFERENCES users(user_id), FOREIGN KEY (payment_type_id) REFERENCES payment_types(payment_type_id))');
