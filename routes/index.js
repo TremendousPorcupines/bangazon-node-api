efbaa5c8c76f43c1ccf20cf3bf64164a0a9c7a9e
Jufebrown <jufebrown@gmail.com>
2017-09-13 14:04:59 -0500

    edited url path for users in index.js

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ const customers = require('./users-route');
 
 router.get('/', (req, res) => {
   res.json({
-    "user": "api/v1/user",
-    "users": "api/v1/users"
+    "user": "/api/v1/user",
+    "users": "/api/v1/users"
   });
 });
 
