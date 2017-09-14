21b3126c1012422a7b9bcdbe4e58bfe868d14976
Jufebrown <jufebrown@gmail.com>
2017-09-14 09:32:07 -0500

    added functionality to getOne and getAll for users and added comments on methods

--- models/user.js
+++ models/user.js
@@ -4,15 +4,29 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/bangazon.sqlite');
 
 const User = {
-  getOne: () => {
-
+  // method for getting one user
+  getOne: (user_id) => {
+    return new Promise( (resolve, reject) => {
+      db.get(`SELECT ${user_id} FROM users`, (err, user) => {
+        if (err) return reject(err);
+        resolve(user);
+      });
+    });
   },
+  // method for getting all users
   getAll: () => {
-
+    return new Promise( (resolve, reject) => {
+      db.all(`SELECT * FROM users`, (err, users) => {
+        if(err) return reject(err);
+        resolve(users); 
+      });
+    });
   },
+  // method for adding a user
   addOne: () => {
     
   },
+  // method for updating a user's info
   edit: () => {
     
   }
