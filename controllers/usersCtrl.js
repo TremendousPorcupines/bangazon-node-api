240aaabc37c63fd39b42350477e6615d086964c8
Jufebrown <jufebrown@gmail.com>
2017-09-13 21:38:47 -0500

    wrote function for getAllUsers

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -7,7 +7,10 @@ module.exports.getOneUser = (req, res, next) => {
 };
 
 module.exports.getAllUsers = (req, res, next) => {
-  ;
+  User.getAll()
+  .then((users) => {
+    res.status(200).json(users);
+  });
 };
 
 module.exports.addNewUser = (req, res, next) => {
