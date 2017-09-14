c80eb76dc8626ae750101b23905e46ba98711612
Jufebrown <jufebrown@gmail.com>
2017-09-13 21:37:03 -0500

    required other methods

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -4,8 +4,16 @@ const User = require('../models/user');
 
 module.exports.getOneUser = (req, res, next) => {
   ;
-}
+};
 
 module.exports.getAllUsers = (req, res, next) => {
   ;
-}
+};
+
+module.exports.addNewUser = (req, res, next) => {
+  ;
+};
+
+module.exports.editUser = (req, res, next) => {
+  ;
+;}
