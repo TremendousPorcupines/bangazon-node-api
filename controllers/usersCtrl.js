f5b49279c419cd7caa50033890f1f53a81c1532f
Jufebrown <jufebrown@gmail.com>
2017-09-13 21:49:08 -0500

    added functionality to getOneUser

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -2,8 +2,11 @@
 
 const User = require('../models/user');
 
-module.exports.getOneUser = (req, res, next) => {
-  ;
+module.exports.getOneUser = ({params: {user_id}}, res, next) => {
+  User.getOne(user_id)
+  .then((user) => {
+    res.status(200).json(user);
+  });
 };
 
 module.exports.getAllUsers = (req, res, next) => {
