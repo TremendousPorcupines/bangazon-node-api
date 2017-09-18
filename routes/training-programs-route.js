492ed19fbd85c29fe48a825e87b9a5a9be7ae193
Greg Korte <gregmkorte@gmail.com>
2017-09-18 15:33:44 -0500

    Added routes for training programs.

---
+++ routes/training-programs-route.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const {Router} = require('express');
+const router = Router();
+const {getOneTrainingProgram, getAllTrainingPrograms, addNewTrainingProgram, editTrainingProgram} = require('../controllers/trainingProgramsCtrl');
+
+router.get('/training_program/:training_program_id', getOneTrainingProgram);
+router.get('/training_programs', getAllTrainingPrograms);
+router.post('/training_program/new', addNewTrainingProgram);
+router.put('/training_program/edit', editTrainingProgram);
+router.delete('/training_program/delete', deleteTrainingProgram);
+
+module.exports = router;
