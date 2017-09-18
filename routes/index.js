1196fabe5a1209d8e72c806aa6661d13fbb8cb01
Jufebrown <jufebrown@gmail.com>
2017-09-18 09:50:48 -0500

    added product routes to index route

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,11 @@ router.get('/', (req, res) => {
     "getUserById": "/api/v1/user/:<user_id>",
     "getAllUsers": "/api/v1/users",
     "addUser": "/api/v1/user/new",
-    "editUser": "/api/v1/user/edit"
+    "editUser": "/api/v1/user/edit",
+    "getProductById": "/api/v1/product/:<product_id>",
+    "getAllProducts": "/api/v1/products",
+    "addProduct": "/api/v1/product/new",
+    "editProduct": "/api/v1/product/edit"
   });
 });
 
