8d6940323720ae717aec5ed6bd2460f501179f42
Jufebrown <jufebrown@gmail.com>
2017-09-18 13:14:36 -0500

    added order routes to index.js

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,19 @@ const {Router} = require('express');
 const router = Router();
 
 router.use( require('./users-route'));
+router.use(require('./orders-route'))
 
 router.get('/', (req, res) => {
   res.json({
     "getUserById": "/api/v1/user/:<user_id>",
     "getAllUsers": "/api/v1/users",
     "addUser": "/api/v1/user/new",
-    "editUser": "/api/v1/user/edit"
+    "editUser": "/api/v1/user/edit",
+    "getOrderById": "/api/v1/order/:<order_id>",
+    "getAllOrders": "/api/v1/orders",
+    "addOrder": "/api/v1/order/new",
+    "editOrder": "/api/v1/order/edit",
+    "deleteOrder": "/api/v1/order/delete/:<order_id>"
   });
 });
 
