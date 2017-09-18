73a06d05766c95e22058810b5b7bf8786ac0a340
Jufebrown <jufebrown@gmail.com>
2017-09-18 15:14:12 -0500

    added employees-route.js

---
+++ routes/employees-route.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const {Router} = require('express');
+const router = Router();
+const {getOneUser, getAllUsers, addNewUser, editUser} = require('../controllers/usersCtrl');
+
+router.get('/user/:user_id', getOneUser);
+router.get('/users', getAllUsers);
+router.post('/user/new', addNewUser);
+router.put('/user/edit', editUser);
+
+module.exports = router;
