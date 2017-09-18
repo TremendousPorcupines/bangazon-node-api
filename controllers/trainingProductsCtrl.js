bffefee0460c9c8a49f272108e41667802d2d3e9
Greg Korte <gregmkorte@gmail.com>
2017-09-18 15:18:32 -0500

    Added training programs controller.

---
+++ controllers/trainingProductsCtrl.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const TrainingProgram = require('../models/training_program');
+
+module.exports.getOneTrainingProgram = ({params: {training_program_id}}, res, next) => {
+  TrainingProgram.getOne(training_program_id)
+  .then((training_program) => {
+    res.status(200).json(training_program);
+  });
+};
+
+module.exports.getAllTrainingPrograms = (req, res, next) => {
+  TrainingProgram.getAll()
+  .then((training_programs) => {
+    res.status(200).json(training_programs);
+  });
+};
+
+module.exports.addNewTrainingProgram = ({body}, res, next) => {
+  TrainingProgram.addOne(body)
+  .then(() => {
+    res.status(200).end();
+  });
+};
+
+module.exports.editTrainingProgram = ({body}, res, next) => {
+  TrainingProgram.edit(body)
+  .then((training_program) => {
+    res.status(200).json(training_program);
+  });
+};
+
+module.exports.editTrainingProgram = ({params}, res, next) => {
+  TrainingProgram.delete(training_program_id)
+  .then((training_program) => {
+    res.status(200).json(training_program);
+  });
+};
