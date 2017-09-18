3fb70bdf70d3f833a98115a262afa0235c2fd250
Jufebrown <jufebrown@gmail.com>
2017-09-18 10:16:53 -0500

    added delete method to product model

--- models/product.js
+++ models/product.js
@@ -22,31 +22,38 @@ const Product = {
       });
     });
   },
-    // method for adding a product
-    addOne: (product) => {
-      return new Promise((resolve, reject) => {
-        db.run(`INSERT INTO products VALUES(null, "${product.name}", "${product.price}", "${product.user_id}", "${product.product_type_id}")`, (err) => {
+  // method for adding a product
+  addOne: (product) => {
+    return new Promise((resolve, reject) => {
+      db.run(`INSERT INTO products VALUES(null, "${product.name}", "${product.price}", "${product.user_id}", "${product.product_type_id}")`, (err) => {
+        if(err) return reject(err);
+        resolve();
+      });
+    })
+  },
+  // method for updating a product's info
+  edit: (product) => {
+    return new Promise((resolve, reject) => {
+      db.run(`UPDATE products SET
+        first_name = "${product.name}",
+        last_name = "${product.price}",
+        address = "${product.user_id}",
+        city = "${product.product_type_id}"`, 
+        (err) => {
           if(err) return reject(err);
           resolve();
         });
-      })
-    },
-    // method for updating a product's info
-    edit: (product) => {
-      return new Promise((resolve, reject) => {
-        db.run(`UPDATE products SET
-          first_name = "${product.name}",
-          last_name = "${product.price}",
-          address = "${product.user_id}",
-          city = "${product.product_type_id}"`, 
-          (err) => {
-            if(err) return reject(err);
-            resolve();
-          });
-      })
-    }
-  };
-
+    })
+  },
+  // method for deleting a specifed product
+  delete: (product_id) => {
+    return new Promise( (resolve, reject) => {
+      db.get(`DELETE * FROM products WHERE product_id = ${product_id}`, (err, product) => {
+        if (err) return reject(err);
+        resolve(product);
+      });
+    });
+  }
 };
 
 module.exports = Product;
