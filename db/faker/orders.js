4ee6d4e46b269a50a7fd22614436afff416d4b81
Greg Korte <gregmkorte@gmail.com>
2017-09-19 08:00:10 -0500

    Added functionality to support PR #39, where orders can have a payment type of null.

--- db/faker/orders.js
+++ db/faker/orders.js
@@ -13,6 +13,8 @@ module.exports.generateOrders = () => {
       let users = data[0];
       let types = data[1];
       let orders = [];
+      let count = 0;
+      let total = 0;
 
       for (let i = 0; i < 200; i++) {
         let user_id;
@@ -25,8 +27,12 @@ module.exports.generateOrders = () => {
         }
 
         types.forEach((type) => {
-          if (user_id === type.user_id) {
-            payment_type_id = type.payment_type_id;
+          if (user_id === type.user_id && count++ % 6 != 0) {
+            if (count++ % 6 != 0) {
+              payment_type_id = type.payment_type_id;
+            } else {
+              payment_type_id = null;
+            }
           }
         })
 
