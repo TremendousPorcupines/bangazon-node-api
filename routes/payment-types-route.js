7c02afe3029aaeec743906a0d160e659fc90d80a
Jufebrown <jufebrown@gmail.com>
2017-09-20 13:51:50 -0500

    made route names plural

--- routes/payment-types-route.js
+++ routes/payment-types-route.js
@@ -4,10 +4,10 @@ const {Router} = require('express');
 const router = Router();
 const {getOnePaymentType, getAllPaymentTypes, addNewPaymentType, editPaymentType, deletePaymentType} = require('../controllers/paymentTypesCtrl');
 
-router.get('/payment_type/:payment_type_id', getOnePaymentType);
+router.get('/payment_types/:payment_type_id', getOnePaymentType);
 router.get('/payment_types', getAllPaymentTypes);
-router.post('/payment_type/new', addNewPaymentType);
-router.put('/payment_type/edit', editPaymentType);
-router.delete('/payment_type/delete/:payment_type_id', deletePaymentType);
+router.post('/payment_types/new', addNewPaymentType);
+router.put('/payment_types/edit', editPaymentType);
+router.delete('/payment_types/delete/:payment_type_id', deletePaymentType);
 
 module.exports = router;
