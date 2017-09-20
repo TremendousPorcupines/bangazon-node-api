dcb9a571a3d39900d80ccad64dc07c9a22e2e083
Jufebrown <jufebrown@gmail.com>
2017-09-20 13:54:54 -0500

    made route names plural

--- routes/users-route.js
+++ routes/users-route.js
@@ -4,9 +4,9 @@ const {Router} = require('express');
 const router = Router();
 const {getOneUser, getAllUsers, addNewUser, editUser} = require('../controllers/usersCtrl');
 
-router.get('/user/:user_id', getOneUser);
+router.get('/users/:user_id', getOneUser);
 router.get('/users', getAllUsers);
-router.post('/user/new', addNewUser);
-router.put('/user/edit', editUser);
+router.post('/users/new', addNewUser);
+router.put('/users/edit', editUser);
 
 module.exports = router;
