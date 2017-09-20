89bee560228c6b5185b435e754b74d887974f2fc
Jufebrown <jufebrown@gmail.com>
2017-09-20 13:51:18 -0500

    made route names plural

--- routes/departments-route.js
+++ routes/departments-route.js
@@ -4,9 +4,9 @@ const {Router} = require('express');
 const router = Router();
 const {getOneDepartment, getAllDepartments, addNewDepartment, editDepartment} = require('../controllers/departmentsCtrl');
 
-router.get('/department/:department_id', getOneDepartment);
+router.get('/departments/:department_id', getOneDepartment);
 router.get('/departments', getAllDepartments);
-router.post('/department/new', addNewDepartment);
-router.put('/department/edit', editDepartment);
+router.post('/departments/new', addNewDepartment);
+router.put('/departments/edit', editDepartment);
 
 module.exports = router;
