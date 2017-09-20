a6e641a8d96fdcb0ad9a227bb03443fcee844d91
Jufebrown <jufebrown@gmail.com>
2017-09-20 13:54:37 -0500

    made route names plural

--- routes/training-programs-route.js
+++ routes/training-programs-route.js
@@ -4,10 +4,10 @@ const {Router} = require('express');
 const router = Router();
 const {getOneTrainingProgram, getAllTrainingPrograms, addNewTrainingProgram, editTrainingProgram, deleteTrainingProgram} = require('../controllers/trainingProgramsCtrl');
 
-router.get('/training_program/:training_program_id', getOneTrainingProgram);
+router.get('/training_programs/:training_program_id', getOneTrainingProgram);
 router.get('/training_programs', getAllTrainingPrograms);
-router.post('/training_program/new', addNewTrainingProgram);
-router.put('/training_program/edit', editTrainingProgram);
-router.delete('/training_program/delete/:training_program_id', deleteTrainingProgram);
+router.post('/training_programs/new', addNewTrainingProgram);
+router.put('/training_programs/edit', editTrainingProgram);
+router.delete('/training_programs/delete/:training_program_id', deleteTrainingProgram);
 
 module.exports = router;
