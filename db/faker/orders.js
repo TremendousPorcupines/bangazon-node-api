7cf57b2e4b1c8a7207282c248326305bec24514d
Greg Korte <gregmkorte@gmail.com>
2017-09-21 11:29:57 -0500

    Changed the way user_id is populated to random, so there is a chance that some users do not have orders.

--- db/faker/orders.js
+++ db/faker/orders.js
@@ -16,14 +16,8 @@ module.exports.generateOrders = () => {
       let count = 0;
 
       for (let i = 0; i < 200; i++) {
-        let user_id;
+        let user_id = Math.floor(Math.random() * users.length) + 1;
         let payment_type_id;
-
-        if (i+1 > users.length) {
-          user_id = Math.floor(Math.random() * users.length) + 1;
-        } else {
-          user_id = i+1;
-        }
 
         types.forEach((type) => {
           if (user_id === type.user_id && count++ % 6 != 0) {
